9c37e70113915586a80a8c96fcd2749c328e43fb
Naveenpradeep02 <naveenpradeep0987@gmail.com>
2026-03-09 21:54:57 +0530

    update

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -18,3 +18,32 @@
 // };
 
 // module.exports = sendOTP;
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+const sendOTP = async (email, otp) => {
+  try {
+    const info = await transporter.sendMail({
+      from: `"Hospital App" <${process.env.EMAIL_USER}>`,
+      to: email,
+      subject: "Your Login OTP",
+      text: `Your verification OTP is: ${otp}`,
+    });
+
+    console.log("Email sent:", info.messageId);
+  } catch (error) {
+    console.error("MAIL ERROR:", error);
+    throw error;
+  }
+};
+
+module.exports = sendOTP;

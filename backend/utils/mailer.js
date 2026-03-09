acecf709e6128ffda259cce96ad61e079718362c
Naveenpradeep02 <naveenpradeep0987@gmail.com>
2026-03-09 22:46:37 +0530

    update

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -53,18 +53,14 @@ const { Resend } = require("resend");
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendOTP = async (email, otp) => {
-  try {
-    const response = await resend.emails.send({
-      from: "onboarding@resend.dev",
-      to: email,
-      subject: "Your Login OTP",
-      html: `<h2>Your OTP is ${otp}</h2>`,
-    });
+  const response = await resend.emails.send({
+    from: "onboarding@resend.dev",
+    to: email,
+    subject: "Your Login OTP",
+    html: `<h2>Your OTP is ${otp}</h2>`,
+  });
 
-    console.log("Email sent:", response);
-  } catch (error) {
-    console.error("MAIL ERROR:", error);
-  }
+  console.log(response);
 };
 
 module.exports = sendOTP;
